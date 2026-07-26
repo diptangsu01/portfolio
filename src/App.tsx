3f3814fdbbd2eec2f7cefbbfd3ba84b3b65389c3
Diptangsu Sasmal <diptangsu.sasmal01@gmail.com>
2026-07-26 16:50:09 +0530

    commented about section import in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@
 
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
-import About from "./components/sections/About";
+// import About from "./components/sections/About";
 import Experience from "./components/sections/Experience";
 
 import Skills from "./components/sections/Skills";
